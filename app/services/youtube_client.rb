bf63282f07e3fd05a77e966580fbf7959695569c
Brandt Lareau <brandt.lareau@rackspace.com>
2017-09-02 16:19:10 -0500

    Added dotenv file to the rails application

--- app/services/youtube_client.rb
+++ app/services/youtube_client.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 class YoutubeClient
   attr_accessor :uri
   def initialize
     self.uri = 'https://www.googleapis.com/youtube/v3'
     self.api_key = ENV['YOUTUBE_API_KEY']
-    if api_key.nil?
-      raise 'YOUTUBE_API_KEY enviorment varable is missing. Won\'t be able to get to youtube'
-    end
+    raise 'YOUTUBE_API_KEY enviorment varable is missing. Won\'t be able to get to youtube' if api_key.nil?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :video_views, only: [:new, :create]
+  resources :video_views, only: %i[new create]
   root to: 'video_views#new'
 end
